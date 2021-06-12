839915c991e4cd3d56dee3987a7cf249e4db3d35
nutboy <nutboy@126.com>
2021-06-12 21:29:41 +0800

    new

--- TensorflowStudy/TestComplete.py
+++ TensorflowStudy/TestComplete.py
@@ -1,22 +1,22 @@
 print(123)
-import numpy as np
+#import numpy as np
 
-def naive_vector_dot(x.y):
-    assert len(x.shape)==1
-    assert len(y.shape)==1
-    assert x.shape[0]==y.shape[0]
-    z=0
-    for i in range(x.shape[0]):
-        z+=x[i]*y[i]
-    return z
-def navie_matrix_vector_dot(x,y):
-    assert len(x.shape)==2
-    assert len(y.shape)==1
-    assert x.shape[1]==y.shape[0]
+#def naive_vector_dot(x.y):
+ #   assert len(x.shape)==1
+  #  assert len(y.shape)==1
+  #  assert x.shape[0]==y.shape[0]
 
-    z=np.zeros(x.shape[0])
-    for i in range(x.shape[0]):
-        for j in range(x.shape[1]):
-            z[i]+=x[i,j]*y[j]
-    return z
-
+  #  for i in range(x.shape[0]):
+  #      z+=x[i]*y[i]
+  #  return z
+#def navie_matrix_vector_dot(x,y):
+ #   assert len(x.shape)==2
+ #   assert len(y.shape)==1
+ #   assert x.shape[1]==y.shape[0]
+#
+ #   z=np.zeros(x.shape[0])
+ #   for i in range(x.shape[0]):
+ #       for j in range(x.shape[1]):
+ #           z[i]+=x[i,j]*y[j]
+ #   return z
+#print(6666)
